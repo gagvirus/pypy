de219f60b14189ee33ca0a30b5e3bf8890d186ef
Gegham Ayvazyan <gegham.aivazyan@fifth-llc.com>
2020-12-09 18:31:09 +0400

    Some random code

---
+++ math.py
@@ -0,0 +1,21 @@
+print(2 ** 2)  # 4
+print(3 ** 2)  # 9
+print(2 ** 5)  # 32
+print(25 ** 0.5)  # 5.0
+print(20 // 6)  # 3
+print(123 // 15)  # 8
+print(20 % 6)  # 2
+
+print("""
+hello
+world
+""")
+
+print("Hello" + " World")
+
+print("I am repeated 5 times\n" * 5)
+
+name = 'Jack'
+print(name)
+name = 25
+print(name)
