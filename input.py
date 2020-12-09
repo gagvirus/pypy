8afa15cdd28ff23b8bc12a8328888b58e7c5a885
Gegham Ayvazyan <gegham.aivazyan@fifth-llc.com>
2020-12-09 18:39:43 +0400

    Another useless commit

---
+++ input.py
@@ -0,0 +1,17 @@
+x = str(input('Wazzzzuuuup\n'))  # convert to string
+
+print(x)
+
+age = int(input('How old are You ? \n'))  # convert to int
+
+# similarly You can convert to float()
+
+print(age)
+
+a = True
+b = False
+
+true = True
+false = False
+
+print("Hello" > "World")
