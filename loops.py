1d1f00a5d6c7e163451869428c43525f32a95afc
Gegham Ayvazyan <gegham.aivazyan@fifth-llc.com>
2020-12-09 19:19:23 +0400

    Sometimes it rains

---
+++ loops.py
@@ -0,0 +1,10 @@
+i = 3
+while i >= 0:
+    print(i)
+    i -= 1
+
+for word in ['a', 'b', 'c', 'd']:
+    print(word)
+
+for char in 'abcd':
+    print(char)
---
+++ range.py
@@ -0,0 +1,7 @@
+print(range(10))  # range(0, 10)
+
+print(list(range(10)))  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+print(list(range(5, 10)))  # [5, 6, 7, 8, 9]
+
+print(list(range(5, 20, 2)))  # [5, 7, 9, 11, 13, 15, 17, 19]
