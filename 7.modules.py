d93ee0acd5f409d953ffbb7b22e90afddbc4a6e1
Gegham Ayvazyan <gegham.aivazyan@fifth-llc.com>
2020-12-09 19:36:13 +0400

    Import-շմimport

---
+++ 7.modules.py
@@ -0,0 +1,20 @@
+import random
+# import math
+# print(math.pi)
+
+# from math import pi
+# print(pi)
+
+# import unknown_module  # ImportError
+
+## Alias-shmalias
+# import math as matem
+# print(matem.pi)
+
+# from math import pi as pipi
+# print(pipi)
+
+
+for i in range(5):
+    value = random.randint(1, 6)
+    print(value)
